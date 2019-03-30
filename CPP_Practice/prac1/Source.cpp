82c64bc577fd3d99c1f438142e69b9a4eed2a3fa
Kota Miura <megalith5836@yahoo.co.jp>
2019-03-30 16:54:24 +0900

    Cinを用いたプログラムの練習

---
+++ CPP_Practice/prac1/Source.cpp
@@ -0,0 +1,16 @@
+// Source.cpp
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(void)
+{
+	string name;
+	cout << "Hello ! I'm Computer!" << endl;
+	cout << "Please input your name" << endl;
+	cin >> name;
+	cout << name << " san! Nice to meet you!" << endl;
+
+	return 0;
+}
