702189e67631318dc894177ff71e2973b7b177ed
Kota Miura <megalith5836@yahoo.co.jp>
2019-03-30 18:00:15 +0900

    オオカミクラスを追加

--- CPP_Practice/prac2-class/Source.cpp
+++ CPP_Practice/prac2-class/Source.cpp
@@ -4,31 +4,50 @@
 #include <string>
 using namespace std;
 
-// 猫を鳴かせるための簡単なクラス
+// 猫クラス
 class Neko
 {
 	string name;	// 猫の名前
 
 public:
-	Neko(string s);			// コンストラクタ
+	Neko(string);			// コンストラクタ
 
-	void naku(void) const;	// 猫を鳴かせるメンバ関数
+	void naku(void) const;	// 猫を鳴かせる
 };
 
-// 関数定義
+// オオカミクラス
+class Wolf
+{
+	string name;	// オオカミの名前
+
+public:
+	Wolf(string);			// コンストラクタ
+
+	void naku(void) const;	// オオカミを吠えさせる
+};
+
+// メンバ関数の定義
 Neko::Neko(string s) :name(s) {}
 void Neko::naku(void) const { cout << name << " : Meow..." << endl; }
 
+Wolf::Wolf(string s) : name(s) { };
+void Wolf::naku(void) const { cout << name << " : Howl..." << endl; }
+
 int main(void)
 {
-	string nekoName;
+	string nekoName, wolfName;
 
-	cout << "猫の名前を入力" << endl;
+	cout << "猫の名前を入力 : ";
 	cin >> nekoName;
 
+	cout << "オオカミの名前を入力 : ";
+	cin >> wolfName;
+
 	Neko MyCat(nekoName);
+	Wolf MyWolf(wolfName);
 
 	MyCat.naku();	// 猫を鳴かせる
+	MyWolf.naku();	// オオカミを吠えさせる
 
 	return 0;
 }
